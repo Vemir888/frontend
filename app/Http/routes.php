85b3e1977bba6ecd8cf4389442e45964362ca2a6
Davit <davstepanyan95@gmail.com>
2016-06-30 00:42:05 +0400

    stocks

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/stocks', function () {
+    return view('pages.stocks');
+});
---
+++ resources/views/pages/stocks.blade.php
@@ -0,0 +1,150 @@
+@extends('layouts.layout')
+
+@section('navbar')
+	<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	    <div class="container">
+	        <!-- Brand and toggle get grouped for better mobile display -->
+	        <div class="navbar-header">
+	            
+	        </div>
+	        <!-- Collect the nav links, forms, and other content for toggling -->
+	        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	            
+	        </div>
+	        <!-- /.navbar-collapse -->
+	    </div>
+	    <!-- /.container -->
+	</nav>
+
+@stop
+
+
+@section('content')
+	<div class="container">
+
+	    <div class="row">
+
+	        
+
+	        <div class="col-md-12">
+
+	            <div class="row carousel-holder">
+
+	                <div class="col-md-12">
+	                	<b>АКЦИИ</b>
+	                	<hr>
+	                    <p>
+	                    	Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+	                    </p>
+	                </div>
+
+	            </div>
+
+	            <div class="row">
+
+	                <div class="col-sm-4 col-lg-4 col-md-4">
+	                    <div class="thumbnail">
+	                        <img src="http://placehold.it/320x150" alt="">
+	                        <div class="caption">
+	                            <b>Lorem Ipsum</b>
+	                            <br><br>
+	                            <p>
+	                            	Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+	                            </p>
+	                        </div>
+	                        <div class="ratings">
+	                            <p></p>
+	                        </div>
+	                    </div>
+	                </div>
+
+	                <div class="col-sm-4 col-lg-4 col-md-4">
+	                    <div class="thumbnail">
+	                        <img src="http://placehold.it/320x150" alt="">
+	                        <div class="caption">
+	                            <b>Lorem Ipsum</b>
+	                            <br><br>
+	                            <p>
+	                            	Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+	                            </p>
+	                        </div>
+	                        <div class="ratings">
+	                            <p></p>
+	                        </div>
+	                    </div>
+	                </div>
+
+	                <div class="col-sm-4 col-lg-4 col-md-4">
+	                    <div class="thumbnail">
+	                        <img src="http://placehold.it/320x150" alt="">
+	                        <div class="caption">
+	                            <b>Lorem Ipsum</b>
+	                            <br><br>
+	                            <p>
+	                            	Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+	                            </p>
+	                        </div>
+	                        <div class="ratings">
+	                            <p></p>
+	                        </div>
+	                    </div>
+	                </div>
+
+	                <div class="col-sm-4 col-lg-4 col-md-4">
+	                    <div class="thumbnail">
+	                        <img src="http://placehold.it/320x150" alt="">
+	                        <div class="caption">
+	                            <b>Lorem Ipsum</b>
+	                            <br><br>
+	                            <p>
+	                            	Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+	                            </p>
+	                        </div>
+	                        <div class="ratings">
+	                            <p></p>
+	                        </div>
+	                    </div>
+	                </div>
+
+	                <div class="col-sm-4 col-lg-4 col-md-4">
+	                    <div class="thumbnail">
+	                        <img src="http://placehold.it/320x150" alt="">
+	                        <div class="caption">
+	                            <b>Lorem Ipsum</b>
+	                            <br><br>
+	                            <p>
+	                            	Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+	                            </p>
+	                        </div>
+	                        <div class="ratings">
+	                            <p></p>
+	                        </div>
+	                    </div>
+	                </div>
+
+	                <div class="col-sm-4 col-lg-4 col-md-4">
+	                    <div class="thumbnail">
+	                        <img src="http://placehold.it/320x150" alt="">
+	                        <div class="caption">
+	                            <b>Lorem Ipsum</b>
+	                            <br><br>
+	                            <p>
+	                            	Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+	                            </p>
+	                        </div>
+	                        <div class="ratings">
+	                            <p></p>
+	                        </div>
+	                    </div>
+	                </div>
+
+	                
+
+	            </div>
+
+	        </div>
+
+	    </div>
+
+	</div>
+@stop
